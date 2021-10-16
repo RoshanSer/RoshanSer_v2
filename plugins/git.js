febe61c05262f56b4d80db8d63d165841556a18e
MohammedRoshanT <70361124+MohammedRoshanT@users.noreply.github.com>
2021-10-16 13:46:04 +0400

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,24 +7,21 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
-var r_text = new Array ();
-
-
-
+    var r_text = new Array ();
+    
+    
+   
   r_text[0] = "https://i.imgur.com/yCYYyEJ.jpeg";
-
-
+    
+    
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Created by Roshan*
-    message.sendMessage(Buffer(), Messagetype.text: `*Created by Roshan*
     *Github:https://github.com/RoshanSer/RoshanSer_v2*
     *Join Whatsapp group:* https://chat.whatsapp.com/KequP491eI1IVVipViyWQP
 
-
-    
 `}) 
 
 }));
