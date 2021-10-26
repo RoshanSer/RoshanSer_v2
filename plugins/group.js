75dc20027b04d3ecea39226a80709a5fc0488b8e
MohammedRoshanT <70361124+MohammedRoshanT@users.noreply.github.com>
2021-10-26 13:55:06 +0400

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -5,7 +5,7 @@ const {WAConnection, MessageOptions, MessageType, Mimetype, Presence} = require(
 const fs = require('fs');
 const axios = require('axios');
 
-Asena.addCommand({pattern: 'support', fromMe: false, desc: 'more txtit commands'}, (async (message, match) => {
+Asena.addCommand({pattern: 'support', fromMe: false, desc: 'Support links'}, (async (message, match) => {
   
   await message.sendMessage('Join *RoshanSer* Support group for updates and doubt clearence\n\n *Group Link*:_https://chat.whatsapp.com/KequP491eI1IVVipViyWQP_');
 
