4898a8dc0778dd5a2566be2f9cf0bb6f809547b8
MohammedRoshanT <70361124+MohammedRoshanT@users.noreply.github.com>
2021-10-27 07:37:02 +0400

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -6,7 +6,11 @@ const fs = require('fs');
 const axios = require('axios');
 
 Asena.addCommand({pattern: 'support', fromMe: false, desc: 'Support links'}, (async (message, match) => {
-  
-  await message.sendMessage('Join *RoshanSer* Support group for updates and doubt clearence\n\n *Group Link*:_https://chat.whatsapp.com/KequP491eI1IVVipViyWQP_');
+  if (Config.GRPLINK == 'https://chat.whatsapp.com/KequP491eI1IVVipViyWQP') {
+            await message.sendMessage(message.jid,'Join *RoshanSer* Support group for updates and doubt clearence\n\n *Group Link*: _https://chat.whatsapp.com/KequP491eI1IVVipViyWQP_ ', MessageType.text);
+       } else {
+            await message.sendMessage(message.jid,'Join',Config.BOTPLK,'Support group for updates and doubt clearence\n\n *Group Link*:',Config.GRPLINK,'\n\n Join *RoshanSer* Group:https://chat.whatsapp.com/KequP491eI1IVVipViyWQP_ ', MessageType.text);
+       
+        }
 
 }));
