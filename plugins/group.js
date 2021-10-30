cc3db407c09099c85130e823bbaf6a18ac83f906
MohammedRoshanT <70361124+MohammedRoshanT@users.noreply.github.com>
2021-10-30 10:46:49 +0400

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -5,12 +5,8 @@ const {WAConnection, MessageOptions, MessageType, Mimetype, Presence} = require(
 const fs = require('fs');
 const axios = require('axios');
 
-Asena.addCommand({pattern: 'support', fromMe: false, desc: 'Support links'}, (async (message, match) => {
-  if (Config.GRPLINK == 'https://chat.whatsapp.com/KequP491eI1IVVipViyWQP') {
-            await message.sendMessage(message.jid,'Join *RoshanSer* Support group for updates and doubt clearence\n\n *Group Link*: _https://chat.whatsapp.com/KequP491eI1IVVipViyWQP_ ', MessageType.text);
-       } else {
-            await message.sendMessage(message.jid,'Join',Config.BOTPLK,'Support group for updates and doubt clearence\n\n *Group Link*:',Config.GRPLINK,'\n\n Join *RoshanSer* Group:https://chat.whatsapp.com/KequP491eI1IVVipViyWQP_ ', MessageType.text);
-       
-        }
-
+Asena.addCommand({pattern: 'support', fromMe: false, desc: 'Support/help group link'}, (async (message, match) => {
+  
+      await message.sendMessage(message.jid,'Join',Config.BOTPLK,'Support group for updates and doubt clearence\n\n *Group Link*:',Config.GRPLINK, MessageType.text);
+   
 }));
